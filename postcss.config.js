109bf9550b4fbfe0fa32b92045d56fafa9360b1f
Yue Tang <yue.tang@farfetch.com>
2022-08-09 15:08:29 +0800

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    plugins: {
+        'postcss-flexbugs-fixes': {},
+        'postcss-preset-env': {
+            'autoprefixer': {
+                'flexbox': 'no-2009'
+            },
+            'stage': 3,
+            'features': {
+                'custom-properties': false
+            }
+        },
+        'postcss-pxtorem': {
+            rootValue: 37.5,
+            unitPrecision: 3,
+            propList: ['*'],
+            selectorBlackList: [/^\.html/], //排除html样式
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0,
+            exclude: /node_modules/i
+        }
+    }
+
+}
